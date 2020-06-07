c1c42b4d7a3e72a2939eb8496fbb2b85ece95cb3
8-lambda-8 <j.wasleqqq@gmail.com>
2020-06-07 22:06:50 +0200

    implemented Car Settings activity

--- app/src/main/java/com/a8lambda8/carlog/CarSettingsActivity.java
+++ app/src/main/java/com/a8lambda8/carlog/CarSettingsActivity.java
@@ -1,17 +1,27 @@
 package com.a8lambda8.carlog;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceFragmentCompat;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.a8lambda8.carlog.myUtils.SP;
+import static com.a8lambda8.carlog.myUtils.currentCarRef;
 
 public class CarSettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.settings_activity);
+        setContentView(R.layout.car_settings_activity);
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.settings, new SettingsFragment())
@@ -20,6 +30,21 @@ public class CarSettingsActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+        FloatingActionButton fab = findViewById(R.id.settingsFab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Map<String, Object> settingsData = new HashMap<>();
+                settingsData.put("name", SP.getString("name",null));
+                settingsData.put("type", SP.getString("type",null));
+                settingsData.put("speedEnabled", SP.getBoolean("speedEnabled",true));
+                settingsData.put("drainEnabled", SP.getBoolean("drainEnabled",true));
+
+                currentCarRef.update(settingsData);
+                Snackbar.make(view, "Changes saved", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
